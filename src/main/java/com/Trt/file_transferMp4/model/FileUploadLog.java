4e07c26abbab1903761a16452d6ac271574f669c
HuseyinErkek <115713946+HuseyinErkek@users.noreply.github.com>
2024-07-09 17:42:37 +0300

    Role user ve fileupload modeller yuklendi

--- src/main/java/com/Trt/file_transferMp4/model/FileUploadLog.java
+++ src/main/java/com/Trt/file_transferMp4/model/FileUploadLog.java
@@ -1,2 +1,84 @@
-package com.Trt.file_transferMp4.model;public class FileUploadLog {
+package com.Trt.file_transferMp4.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class FileUploadLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String fileName;
+    private LocalDateTime uploadTime;
+    private String filePath;
+    private int ip;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Constructor
+    public FileUploadLog() {
+        // default constructor
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public LocalDateTime getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(LocalDateTime uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public int getIp() {
+        return ip;
+    }
+
+    public void setIp(int ip) {
+        this.ip = ip;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/Trt/file_transferMp4/model/Role.java
+++ src/main/java/com/Trt/file_transferMp4/model/Role.java
@@ -1,2 +1,23 @@
-package com.Trt.file_transferMp4.model;public class Role {
-}
+package com.Trt.file_transferMp4.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+
+    @Entity
+    public class Role {
+        @Getter
+        @Setter
+        @jakarta.persistence.Id
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+
+        // getters and setters
+    }
+
