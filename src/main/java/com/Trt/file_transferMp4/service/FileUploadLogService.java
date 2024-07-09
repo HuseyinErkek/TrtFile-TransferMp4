78d02123704ef395534e7ce5345f884091d583ca
HuseyinErkek <115713946+HuseyinErkek@users.noreply.github.com>
2024-07-09 17:41:18 +0300

    Filelogservis ve user servvice yuklendi.

--- src/main/java/com/Trt/file_transferMp4/service/FileUploadLogService.java
+++ src/main/java/com/Trt/file_transferMp4/service/FileUploadLogService.java
@@ -1,2 +1,30 @@
-package com.Trt.file_transferMp4.service;public class FileUploadLogService {
+package com.Trt.file_transferMp4.service;
+
+import com.Trt.file_transferMp4.model.FileUploadLog;
+import com.Trt.file_transferMp4.model.User;
+import com.Trt.file_transferMp4.repository.FileUploadLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FileUploadLogService {
+
+    @Autowired
+    private FileUploadLogRepository fileUploadLogRepository;
+
+    public void saveLog(FileUploadLog log) {
+        fileUploadLogRepository.save(log);
+    }
+
+    public List<FileUploadLog> findLogsByUser(User user) {
+        return fileUploadLogRepository.findByUser(user);
+    }
+
+    public List<FileUploadLog> findAllLogs() {
+        return fileUploadLogRepository.findAll();
+    }
+
+    // Diğer servis metodları
 }
